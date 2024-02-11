ed2d42979e960c7044924b07d8044a060bfcd820
sylvaincodes <sylvaincodeur@gmail.com>
2024-02-11 10:17:02 +0100

    modal form: set up great

--- components/ui/modal.tsx
+++ components/ui/modal.tsx
@@ -1,43 +1,42 @@
-"use client"
+"use client";
 
-import { ReactNode } from "react";
-import {
-    Dialog,
-    DialogContent,
-    DialogDescription,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger,
-  } from "@/components/ui/dialog"
-interface ModalProps{
-    title: string;
-    description: string;
-    isOpen: boolean;
-    onClose: ()=> void;
-    children?: ReactNode
+import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle } from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+
+interface ModalProps {
+  title: string;
+  description: string;
+  isOpen: boolean;
+  onClose: () => void;
+  children?: React.ReactNode;
 }
 
-
-export const Modal: React.FC<ModalProps> = ({ title,description,isOpen,onClose,children}) => {
-
-    const onChange = (open: boolean) => {
-      if(!open){
-        onClose()
-      }
+export const Modal: React.FC<ModalProps> = ({
+  title,
+  description,
+  isOpen,
+  onClose,
+  children
+}) => {
+  const onChange = (open: boolean) => {
+    if (!open) {
+      onClose();
     }
-    
-  return (
-    <Dialog open={isOpen} onOpenChange={onChange}>
-        <DialogTrigger>Open</DialogTrigger>
-        <DialogContent>
-            <DialogHeader>
-            <DialogTitle>{title}</DialogTitle>
-            <DialogDescription>
-                {description}
-            </DialogDescription>
-            </DialogHeader>
-        </DialogContent>
-    </Dialog>
-  )
-}
+  };
 
+  return ( 
+    <Dialog open={isOpen} onOpenChange={onChange}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>{title}</DialogTitle>
+          <DialogDescription>
+            {description}
+          </DialogDescription>
+        </DialogHeader>
+        <div>
+          {children}
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+};
