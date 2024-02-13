edc8e644456b5a195815de5bc5e5a808025dba31
sylvaincodes <sylvaincodeur@gmail.com>
2024-02-13 18:07:02 +0100

    update marketing page

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,8 +8,23 @@ import { ToastProvider } from "@/providers/toast-provider";
 import { josephan } from "./fonts";
 
 export const metadata: Metadata = {
-  title: "api-store - Multi vendor",
-  description: "api store : Running multiple stores from a single dashboard. ",
+  title: "Api-store - Multi vendor",
+  description:
+    "A platform with built-in NextJs, which helps programmers create eCommerce websites.",
+  applicationName: "API-STORE",
+  authors: ["sylvaincodes"],
+  openGraph: {
+    type: "website",
+    url: "https://apistore-m6uz4upqg-sylvaincodes-projects.vercel.app/home",
+    description:
+      "A platform with built-in NextJs, which helps programmers create eCommerce websites",
+    siteName: "API-STORE",
+    images: [
+      {
+        url: "https://apistore-m6uz4upqg-sylvaincodes-projects.vercel.app/psd.png",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
