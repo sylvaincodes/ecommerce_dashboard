935efbdea998669f0a8d9f819819d0ce7aa0c31c
sylvaincodes <sylvaincodeur@gmail.com>
2024-02-18 12:03:16 +0100

    code review

--- middleware.ts
+++ middleware.ts
@@ -1,23 +1,5 @@
 import { authMiddleware } from "@clerk/nextjs";
- import { NextResponse } from "next/server";
-
-export function middleware() {
-    // retrieve the current response
-    const res = NextResponse.next()
-
-    // add the CORS headers to the response
-    res.headers.append('Access-Control-Allow-Credentials', "true")
-    res.headers.append('Access-Control-Allow-Origin', '*') // replace this your actual origin
-    res.headers.append('Access-Control-Allow-Methods', 'GET,DELETE,PATCH,POST,PUT')
-    res.headers.append(
-        'Access-Control-Allow-Headers',
-        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-    )
-
-    return res
-}
-
-
+ 
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
